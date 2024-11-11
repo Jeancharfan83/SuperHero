332153e8874646da744b4c0f7b5e0a899c99097a
curso2024-git <curso202410@gmail.com>
2024-11-11 17:16:33 +0100

    Empty project

--- app/src/main/java/com/example/superhero/MainActivity.kt
+++ app/src/main/java/com/example/superhero/MainActivity.kt
@@ -5,16 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+//para la api token: Jeancharfan83/SuperHeroes
+//paera la api dirección Jeancharfan83/SuperHeroes
+// web https://superheroapi.com/
+//Postman
+//JSON Editor on line
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
 }
